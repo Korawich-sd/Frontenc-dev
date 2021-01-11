0a614e2cfab5d0e6be62d82d76bd284eed1ccd75
Korawich-sd <76939243+Korawich-sd@users.noreply.github.com>
2021-01-11 15:53:41 +0700

    addicon

--- index.php
+++ index.php
@@ -7,13 +7,14 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="js/jquery-3.5.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>            
-   
-    
+   <link rel="stylesheet" href="icon/css/all.css">
+
     <link rel="stylesheet" href="css/mystyle.css">
  <script src="js/default.js"></script> 
 </head>
 <body>
     <div class="container">
+    <i class="fab fa-apple-pay"></i>
        hello wolee <br>
     <img src="img/profile.jpg" width="200px" alt="" srcset=""> <hr>
         <button class="btn btn-primary"> กดดู </button>
