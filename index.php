5da7ffc4624c9de10a180d0ed9c0dbebe26d87e1
Korawich-sd <76939243+Korawich-sd@users.noreply.github.com>
2021-01-11 16:00:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,19 +6,20 @@
     <title>Document</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="js/jquery-3.5.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>            
-   <link rel="stylesheet" href="icon/css/all.css">
+    <script src="js/bootstrap.min.js"></script>   
+             
+    <link rel="stylesheet" href="icon/css/all.css">
 
     <link rel="stylesheet" href="css/mystyle.css">
  <script src="js/default.js"></script> 
 </head>
 <body>
     <div class="container">
-    <i class="fab fa-apple-pay"></i>
-       hello wolee <br>
+     hello wolee <br>
     <img src="img/profile.jpg" width="200px" alt="" srcset=""> <hr>
-        <button class="btn btn-primary"> กดดู </button>
-        <button class="btn btn-primary"> กดอีก </button>
+   
+        <button class="btn btn-primary"><i class="fas fa-ad"></i> กดดู </button>  <!-- แทรกicon-->
+        <button class="btn btn-primary"> <i class="fas fa-ad"></i>กดอีก </button>
     </div>
  
     <script>
